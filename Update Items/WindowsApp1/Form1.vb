c64024bbeda02a74ab61ae284445c074c6c54a15
shabanelmogy <shaban.prog2021@gmail.com>
2021-11-20 15:05:29 +0200

    Update

--- Update Items/WindowsApp1/Form1.vb
+++ Update Items/WindowsApp1/Form1.vb
@@ -81,7 +81,7 @@ Public Class Form1
         End If
     End Sub
 
-    Private Sub BtnSaveSql_Click(sender As Object, e As EventArgs) Handles BtnSaveSql.Click
+    Sub InsertData()
 
         Dim Connsql As New SqlConnection("Data source=" & CboServer.Text & ";initial catalog=" & CboData.Text & ";user id=sa;password=togetherforever")
 
@@ -370,8 +370,311 @@ Public Class Form1
             MsgBox("تم تحديث بيانات " & i & " " & "صنف", MessageBoxIcon.Information, Title:="Items Updated")
         Else
             MsgBox("تأكد  من إدخال اسم السيرفر وقاعدة البيانات واختيار الملف المطلوب", MessageBoxIcon.Information, Title:="Update Items")
+        End If
+
+    End Sub
+
+    Private Sub BtnSaveSql_Click(sender As Object, e As EventArgs) Handles BtnSaveSql.Click
+
+        If MsgBox("يفضل اخد باكب قبل التحديث", MessageBoxIcon.Information + vbYesNo, "backup") = vbYes Then
+            Btn_Backup.PerformClick()
+            InsertData()
+        Else
+            InsertData()
 
         End If
+
+        'Dim Connsql As New SqlConnection("Data source=" & CboServer.Text & ";initial catalog=" & CboData.Text & ";user id=sa;password=togetherforever")
+
+        'If CboServer.Text <> "" And CboData.Text <> "" And TextFileName.Text <> "" Then
+        '    If MsgBox("يفضل اخد باكب قبل التحديث", MessageBoxIcon.Information + vbYesNo, "backup") = vbYes Then
+        '        Btn_Backup.PerformClick()
+
+        '        Dim i As Integer = 0
+        '        ProgressBar1.Maximum = DataGridView1.Rows.Count - 1
+
+        '        For Each row As DataGridViewRow In DataGridView1.Rows
+
+        '            DataGridView1.AllowUserToAddRows = False
+
+        '            Dim CmdInsertSign As New SqlCommand(" If Not Exists(Select Sign From Items Where Sign = @Sign) 
+        '        Begin Insert Into Items (Sign)  Select @Sign Where @Sign<>'' End ", Connsql)
+
+        '            CmdInsertSign.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+
+        '            '========================================================================================================          
+        '            Dim CmdUsed As New SqlCommand("Update Items Set Used='True' Where Sign=@Sign", Connsql)
+        '            CmdUsed.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+
+        '            '========================================================================================================
+
+        '            Dim CmdName As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items Set Name=@Name Where Sign=@Sign And @Name<>'' End ", Connsql)
+        '            Try
+        '                If Chk_Name.Checked = True Then
+        '                    CmdName.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdName.Parameters.AddWithValue("Name", row.Cells("Name").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم إستيراد بيانات إسم الصنف / راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Error, "تنبيه")
+        '                Exit Sub
+        '            End Try
+        '            '========================================================================================================
+
+        '            Dim CmdItemclassid As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items Set Itemclassid=@Itemclassid Where Sign=@Sign And @Itemclassid>0 End ", Connsql)
+        '            Try
+        '                If Chk_ItemClassid.Checked = True Then
+        '                    CmdItemclassid.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdItemclassid.Parameters.AddWithValue("Itemclassid", row.Cells("Itemclassid").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم إستيراد بيانات رقم مجموعة الصنف / راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Error, "تنبيه")
+        '                Exit Sub
+        '            End Try
+
+        '            '========================================================================================================
+
+        '            Dim CmdMainUnit As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items set MainUnit=@MainUnit Where Sign=@Sign and @MainUnit<>'' end ", Connsql)
+        '            Try
+        '                If Chk_MainUnit.Checked = True Then
+        '                    CmdMainUnit.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdMainUnit.Parameters.AddWithValue("MainUnit", row.Cells("MainUnit").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم إستيراد بيانات الوحدة الرئيسية/ راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Error, "تنبيه")
+        '                Exit Sub
+        '            End Try
+
+        '            '========================================================================================================
+
+        '            Dim CmdSubUnit As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items set SubUnit=@SubUnit Where Sign=@Sign and @SubUnit<>'' end ", Connsql)
+        '            Try
+        '                If Chk_Subunit.Checked = True Then
+        '                    CmdSubUnit.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdSubUnit.Parameters.AddWithValue("SubUnit", row.Cells("SubUnit").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم إستيراد بيانات الوحدة الفرعية/ راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Error, "تنبيه")
+        '                Exit Sub
+        '            End Try
+        '            '========================================================================================================
+
+        '            Dim CmdMainUnitpack As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items set MainUnitPack=@MainUnitPack Where Sign=@Sign and @MainUnitPack>0 end ", Connsql)
+        '            Try
+        '                If Chk_MainUnitpack.Checked = True Then
+        '                    CmdMainUnitpack.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdMainUnitpack.Parameters.AddWithValue("MainUnitPack", row.Cells("MainUnitPack").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم إستيراد بيانات كمية الوحدة الفرعية/ راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Error, "تنبيه")
+        '                Exit Sub
+        '            End Try
+
+        '            '========================================================================================================
+
+        '            Dim CmdSellprice1 As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items set SellPrice1=@Sellprice1 Where Sign=@Sign And @SellPrice1>0 end ", Connsql)
+        '            Try
+        '                If Chk_Sellprice1.Checked = True Then
+        '                    CmdSellprice1.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdSellprice1.Parameters.AddWithValue("SellPrice1", row.Cells("SellPrice1").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم إستيراد بيانات قائمة أسعار 1/ راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Error, "تنبيه")
+        '                Exit Sub
+        '            End Try
+        '            '========================================================================================================
+        '            Dim CmdSellprice2 As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items set SellPrice2=@Sellprice2 Where Sign=@Sign And @SellPrice2>0 end ", Connsql)
+
+        '            Try
+        '                If Chk_Sellprice2.Checked = True Then
+        '                    CmdSellprice2.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdSellprice2.Parameters.AddWithValue("SellPrice2", row.Cells("SellPrice2").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم إستيراد بيانات قائمة أسعار 2 / راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Error, "تنبيه")
+        '                Exit Sub
+        '            End Try
+        '            '=========================================================================================================
+        '            Dim CmdIstoried As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items Set Istoreid=@Istoreid Where Sign=@Sign And @Istoreid>0 End ", Connsql)
+
+        '            Try
+        '                If Chk_Istoried.Checked = True Then
+        '                    CmdIstoried.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdIstoried.Parameters.AddWithValue("Istoreid", row.Cells("Istoreid").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم استيراد بيانات مخزن الصنف / راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Exclamation, "تنبيه")
+        '                Exit Sub
+        '            End Try
+        '            '=========================================================================================================
+        '            Dim CmdTax As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items Set SaleTaxPer=@SaleTaxPer Where Sign=@Sign And @SaleTaxPer>0 End ", Connsql)
+
+        '            Try
+        '                If Chk_Tax.Checked = True Then
+        '                    CmdTax.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdTax.Parameters.AddWithValue("SaleTaxPer", row.Cells("SaleTaxPer").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم استيراد بيانات ضريبة الصنف / راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Exclamation, "تنبيه")
+        '                Exit Sub
+        '            End Try
+        '            '==========================================================================================================
+        '            Dim Cmdsellprice3 As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items Set Sellprice3=@Sellprice3 Where Sign=@Sign And @Sellprice3>0 End ", Connsql)
+
+        '            Try
+        '                If Chk_sellprice3.Checked = True Then
+        '                    Cmdsellprice3.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    Cmdsellprice3.Parameters.AddWithValue("Sellprice3", row.Cells("Sellprice3").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم استيراد بيانات قائمة أسعار 3/ راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Exclamation, "تنبيه")
+        '                Exit Sub
+        '            End Try
+        '            '===========================================================================================================
+        '            Dim CmdPitemtype As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items Set Pitemtype=@Pitemtype Where Sign=@Sign And @Pitemtype between 0 and 2 End ", Connsql)
+
+        '            Try
+        '                If Chk_Pitemtype.Checked = True Then
+        '                    CmdPitemtype.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdPitemtype.Parameters.AddWithValue("Pitemtype", row.Cells("Pitemtype").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم استيراد بيانات نوع الصنف / راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Exclamation, "تنبيه")
+        '                Exit Sub
+        '            End Try
+        '            '===========================================================================================================
+        '            Dim CmdBarcode As New SqlCommand("If Exists(Select Sign From Items Where Sign=@Sign) 
+        '        Begin Update Items Set Barcode=@Barcode Where Sign=@Sign And @Barcode <>'' End ", Connsql)
+
+        '            Try
+        '                If Chk_Barcode.Checked = True Then
+        '                    CmdBarcode.Parameters.AddWithValue("Sign", row.Cells("Sign").Value.ToString)
+        '                    CmdBarcode.Parameters.AddWithValue("Barcode", row.Cells("Barcode").Value.ToString)
+        '                End If
+        '            Catch ex As Exception
+        '                MsgBox("لم يتم استيراد بيانات الباركود الرئيسى / راجع ملف الاكسل طبقا للنموذج المرفق", MessageBoxIcon.Exclamation, "تنبيه")
+        '                Exit Sub
+        '            End Try
+        '            '===========================================================================================================
+        '            Connsql.Open()
+
+        '            CmdInsertSign.ExecuteNonQuery()
+
+        '            Try
+        '                CmdUsed.ExecuteNonQuery()
+        '                i += 1
+        '                ProgressBar1.Value = i
+        '                If ProgressBar1.Value = ProgressBar1.Maximum Then
+        '                    ProgressBar1.Value = 0
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_Name.Checked = True Then
+        '                    CmdName.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_ItemClassid.Checked = True Then
+        '                    CmdItemclassid.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_MainUnit.Checked = True Then
+        '                    CmdMainUnit.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_Subunit.Checked = True Then
+        '                    CmdSubUnit.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_MainUnitpack.Checked = True Then
+        '                    CmdMainUnitpack.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_Sellprice1.Checked = True Then
+        '                    CmdSellprice1.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_Sellprice2.Checked = True Then
+        '                    CmdSellprice2.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_Istoried.Checked = True Then
+        '                    CmdIstoried.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+
+        '            Try
+        '                If Chk_Tax.Checked = True Then
+        '                    CmdTax.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_sellprice3.Checked = True Then
+        '                    Cmdsellprice3.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_Pitemtype.Checked = True Then
+        '                    CmdPitemtype.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Try
+        '                If Chk_Barcode.Checked = True Then
+        '                    CmdBarcode.ExecuteNonQuery()
+        '                End If
+        '            Catch ex As Exception
+        '            End Try
+
+        '            Connsql.Close()
+
+        '            '========================================================================================================
+        '        Next
+
+        '        MsgBox("تم تحديث بيانات " & i & " " & "صنف", MessageBoxIcon.Information, Title:="Items Updated")
+        '    Else
+        '        MsgBox("تأكد  من إدخال اسم السيرفر وقاعدة البيانات واختيار الملف المطلوب", MessageBoxIcon.Information, Title:="Update Items")
+        '    End If
+        'End If
 
     End Sub
 
